a21f414f37684a0da64b0467eb12560625ace961
hjKwak <velmute01@gmail.com>
2023-06-22 18:40:08 +0900

    시간 자릿수 표시

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -5,7 +5,7 @@ export const getTimeStamp = () => {
 }
 
 export const secToTime = (duration: number) => {
-    const minutes = Math.floor(duration / 60)
-    const seconds = Math.floor(duration % 60)
+    const minutes = String(Math.floor(duration / 60)).padStart(2, '0')
+    const seconds = String(Math.floor(duration % 60)).padStart(2, '0')
     return [minutes, seconds]
 }
